848a4df96b2fce5aa09c5075ba83debf0be9f30c
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-05-21 20:44:08 +0530

    Initail commit and starting the project

---
+++ app/components/Click.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+
+
+const Click: React.FC = () => {
+    const handleClick = () => {
+        alert('You clicked the button!')
+    }
+
+    return (
+        <button onClick={handleClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started
+        </button>
+    )
+}
+
+export default Click
---
+++ app/test-page/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const page = () => {
+    return (
+        <>
+            <div className='my-10'>page
+                <Link href={'/'}>Home Page</Link>
+            </div>
+
+        </>
+
+    )
+}
+
+export default page
