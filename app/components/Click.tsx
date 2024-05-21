4bbc2d6ec318f6253954c21bbbaf309de1481846
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-05-21 21:37:54 +0530

    Configuring layout file

--- app/components/Click.tsx
+++ app/components/Click.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 
-const Click: React.FC = () => {
+const Click = () => {
     const handleClick = () => {
         alert('You clicked the button!')
     }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -15,6 +18,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 };
 export default config;
